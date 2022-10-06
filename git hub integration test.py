d287cde672d3972820df813c7bc30b8b1f17a449
dhanudhanush2998@gmail.com <dhanudhanush2998@gmail.com>
2022-10-06 16:00:27 +0000

    commit-test-4

--- git hub integration test.py
+++ git hub integration test.py
@@ -4,3 +4,13 @@
 # COMMAND ----------
 
 hello databricks
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC git rev-parse HEAD
+
+# COMMAND ----------
+
+api_url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().get()
+host_token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
